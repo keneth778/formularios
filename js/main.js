8e3a857f159fe941ad8e7f4a82768bbbdc440b37
keneth <keneth.javier90@gmail.com>
2024-07-24 11:02:38 -0600

    configuracion-general

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-let root = document.querySelector("#root");
+document.addEventListener("DOMContentLoaded", () => {
+    let root = document.querySelector("#root");
 
+    let div_formulario = document.createElement("div");
+    div_formulario.className = "div_formularios";
+    let formularios = [
+        {nombre: "login", link: "componentes/login/login.html"},
+        {nombre: "login", link: "#"},
+        {nombre: "login", link: "#"},
+        {nombre: "login", link: "#"},
+    ];
 
-let div_formulario= document.createElement("div");
-div_formulario.className = "div_formularios";
-let formularios = [
-    {nombre:"login",link:https},
-    {nombre:"login",link:https},
-    {nombre:"login",link:https},
-];
+    formularios.forEach(element => {
+        let div_item = document.createElement("a");
+        div_item.className = "item";
+        div_item.href = element.link;
+        div_item.innerText = element.nombre;
 
-formularios.forEach(Element=>{
+        div_formulario.appendChild(div_item);
+    });
 
-    let div_item = document.createElement("a");
-    div_item.className= "item";
-    div_item.href = item_lista.link;
-    div_item.innerText=item_lista.nombre;
-
-    div_formulario.appendChild(div_item);
-
+    root.appendChild(div_formulario);
 });
-
-root.appendChild(div_formulario);
-
